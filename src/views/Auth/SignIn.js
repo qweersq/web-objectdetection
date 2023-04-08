1bd3ed08a227fc0ad8104a83aef86e516f73f464
Andreean26 <adam.andreean@binus.ac.id>
2023-04-08 23:44:23 +0700

    edit auth sign in

--- src/views/Auth/SignIn.js
+++ src/views/Auth/SignIn.js
@@ -14,7 +14,7 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 // Assets
-import signInImage from "assets/img/signInImage.png";
+import shield2 from "assets/img/shield 2.png";
 
 function SignIn() {
   // Chakra color mode
@@ -30,28 +30,30 @@ function SignIn() {
         justifyContent='space-between'
         mb='30px'
         pt={{ sm: "100px", md: "0px" }}>
+        
         <Flex
           alignItems='center'
           justifyContent='start'
           style={{ userSelect: "none" }}
           w={{ base: "100%", md: "50%", lg: "42%" }}>
+            
           <Flex
             direction='column'
             w='100%'
             background='transparent'
             p='48px'
             mt={{ md: "150px", lg: "80px" }}>
-            <Heading color={titleColor} fontSize='32px' mb='10px'>
-              Welcome Back
+            <Box bg="#fff" p={4} color="black" borderRadius="lg" boxShadow="md" h="505px" w="585px" top="239px" left="464px" >
+           
+            <Heading color={titleColor} fontFamily="Inter" fontStyle="normal" fontSize='32px' mb='10px' alignItems='center' display="flex" justifyContent="center" >
+            <image bg={shield2}
+            w='46px'
+            h='46px'
+            left='569px'
+            top='288px'
+            position='absolute'></image>
+              Object Detection
             </Heading>
-            <Text
-              mb='36px'
-              ms='4px'
-              color={textColor}
-              fontWeight='bold'
-              fontSize='14px'>
-              Enter your email and password to sign in
-            </Text>
             <FormControl>
               <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
                 Email
@@ -75,16 +77,7 @@ function SignIn() {
                 placeholder='Your password'
                 size='lg'
               />
-              <FormControl display='flex' alignItems='center'>
-                <Switch id='remember-login' colorScheme='teal' me='10px' />
-                <FormLabel
-                  htmlFor='remember-login'
-                  mb='0'
-                  ms='1'
-                  fontWeight='normal'>
-                  Remember me
-                </FormLabel>
-              </FormControl>
+              
               <Button
                 fontSize='10px'
                 type='submit'
@@ -116,24 +109,11 @@ function SignIn() {
                 </Link>
               </Text>
             </Flex>
+            </Box>
+            
           </Flex>
         </Flex>
-        <Box
-          display={{ base: "none", md: "block" }}
-          overflowX='hidden'
-          h='100%'
-          w='40vw'
-          position='absolute'
-          right='0px'>
-          <Box
-            bgImage={signInImage}
-            w='100%'
-            h='100%'
-            bgSize='cover'
-            bgPosition='50%'
-            position='absolute'
-            borderBottomLeftRadius='20px'></Box>
-        </Box>
+        
       </Flex>
     </Flex>
   );
