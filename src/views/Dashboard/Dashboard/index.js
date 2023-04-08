9225bdc29108e9bfed6da5adb77fff07c8e175bb
FaridzMuhammad <muhammad.faridz.7545@gmail.com>
2023-04-08 23:06:58 +0700

    Alarm sensorDashboard

--- src/views/Dashboard/Dashboard/index.js
+++ src/views/Dashboard/Dashboard/index.js
@@ -39,7 +39,7 @@ import Bell from "assets/img/Bell.png";
 
 export default function Dashboard() {
   const iconBoxInside = useColorModeValue("white", "white");
-  const pickerStyle = { border: '1px solid #000', borderRadius: '4px', height: '32px', width: '200px', margin: '15px' };
+  const pickerStyle = { border: '1px solid #000', borderRadius: '4px', height: '29px', width: '200px', margin: '15px' };
   const [isOn, setIsOn] = useState(true);
 
 
@@ -51,9 +51,9 @@ export default function Dashboard() {
 
 
   return (
-    <Flex flexDirection='row' justify="space-between" pt={{ base: "120px", md: "75px" }}>
-      <Box bg="#fff" p={4} color="black" borderRadius="lg" boxShadow="md">
-        <Flex justify="space-between" align="baseline" flexDirection="row" minHeight="30px" w="100%">
+    <Grid templateColumns="repeat(2, 1fr)" gap={2} pt={{ base: "120px", md: "75px" }}>
+      <Box bg="#fff" p={4} color="black" borderRadius="lg" boxShadow="md" width="100%">
+        <Flex justifyContent="space-between" alignItems="center">
           <Text> Alarm Sensor</Text>
           <TimePicker.RangePicker format="HH:mm" style={pickerStyle} className="custom-time-picker" />
           <Button
@@ -63,22 +63,22 @@ export default function Dashboard() {
             borderRadius="lg"
             onClick={toggleButton}
             width="77px"
-            height="32px"
+            height="29px"
           >
             {isOn ? 'On' : 'Off'}
           </Button>
         </Flex>
       </Box>
-      <Box bg="#fff" p={4} color="black" borderRadius="lg" boxShadow="md">
-        <Flex justify="space-between" align="baseline" flexDirection="row" minHeight="30px" w="100%">
+      <Box bg="#fff" p={4} color="black" borderRadius="lg" boxShadow="md" width="100%" >
+        <Flex justifyContent="space-between" alignItems="center" pt="14px">
           <Image src={Bell}></Image>
-          <Text size="md"> 124 </Text>
+          <Text textDecor="bold"> 124 </Text>
           <Text size="md"> Sensor Point </Text>
-          <Box bg="tomato" w="77px" h="32px" p={4} color="white">
-            <Text> 124 </Text>
-          </Box>
+          <Button bg="#00DC7F" color="white" _hover="none" width="77px" height="29px" borderRadius="lg">
+            Active
+          </Button>
         </Flex>
       </Box>
-    </Flex>
+    </Grid >
   );
 }
