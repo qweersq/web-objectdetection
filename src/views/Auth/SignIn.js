43aa1fb79c26dce9c3bf06cae0e821760cf61661
Andreean26 <adam.andreean@binus.ac.id>
2023-04-12 22:51:28 +0700

    Sign in

--- src/views/Auth/SignIn.js
+++ src/views/Auth/SignIn.js
@@ -21,99 +21,110 @@ function SignIn() {
   const titleColor = useColorModeValue("teal.300", "teal.200");
   const textColor = useColorModeValue("gray.400", "white");
   return (
-    <Flex position='relative' mb='40px'>
+    <Flex position="relative" mb="40px">
       <Flex
         h={{ sm: "initial", md: "75vh", lg: "85vh" }}
-        w='100%'
-        maxW='1044px'
-        mx='auto'
-        justifyContent='space-between'
-        mb='30px'
+        w="100%"
+        maxW="1044px"
+        mx="auto"
+        justifyContent="space-between"
+        mb="30px"
         pt={{ sm: "100px", md: "0px" }}>
-        
+
         <Flex
-          alignItems='center'
-          justifyContent='start'
+          alignItems="center"
+          justifyContent="start"
           style={{ userSelect: "none" }}
           w={{ base: "100%", md: "50%", lg: "42%" }}>
-            
+
           <Flex
-            direction='column'
-            w='100%'
-            background='transparent'
-            p='48px'
+            direction="column"
+            w="100%"
+            background="transparent"
+            p="48px"
             mt={{ md: "150px", lg: "80px" }}>
-            <Box bg="#fff" p={4} color="black" borderRadius="lg" boxShadow="md" h="505px" w="585px" top="239px" left="464px" >
-           
-            <Heading color={titleColor} fontFamily="Inter" fontStyle="normal" fontSize='32px' mb='10px' alignItems='center' display="flex" justifyContent="center" >
-            <image bg={shield2}
-            w='46px'
-            h='46px'
-            left='569px'
-            top='288px'
-            position='absolute'></image>
-              Object Detection
-            </Heading>
-            <FormControl>
-              <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
-                Email
-              </FormLabel>
-              <Input
-                borderRadius='15px'
-                mb='24px'
-                fontSize='sm'
-                type='text'
-                placeholder='Your email adress'
-                size='lg'
-              />
-              <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
-                Password
-              </FormLabel>
-              <Input
-                borderRadius='15px'
-                mb='36px'
-                fontSize='sm'
-                type='password'
-                placeholder='Your password'
-                size='lg'
-              />
-              
-              <Button
-                fontSize='10px'
-                type='submit'
-                bg='teal.300'
-                w='100%'
-                h='45'
-                mb='20px'
-                color='white'
-                mt='20px'
-                _hover={{
-                  bg: "teal.200",
-                }}
-                _active={{
-                  bg: "teal.400",
-                }}>
-                SIGN IN
-              </Button>
-            </FormControl>
-            <Flex
-              flexDirection='column'
-              justifyContent='center'
-              alignItems='center'
-              maxW='100%'
-              mt='0px'>
-              <Text color={textColor} fontWeight='medium'>
-                Don't have an account?
-                <Link color={titleColor} as='span' ms='5px' fontWeight='bold'>
-                  Sign Up
-                </Link>
-              </Text>
-            </Flex>
+
+            <Box
+              bg="#fff"
+              p={4}
+              alignItems="center"
+              justifyContent="center"
+              color="black"
+              borderRadius="lg"
+              boxShadow="md"
+              h="505px"
+              w="585px"
+              top="239px"
+              left="464px">
+
+              <Heading
+                color={titleColor}
+                fontFamily="Inter"
+                fontStyle="normal"
+                fontSize="32px"
+                mb="10px"
+                alignItems="center"
+                display="flex"
+                justifyContent="center">
+
+                <img
+                  src={shield2}
+                  alt="shield2"
+                  w="46px"
+                  h="46px"
+                  left="569px"
+                  top="288px"
+                  position="absolute"></img>
+                Object Detection
+              </Heading>
+              <FormControl>
+                <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                  Email
+                </FormLabel>
+
+                <Input
+                  borderRadius="15px"
+                  mb="24px"
+                  fontSize="sm"
+                  type="text"
+                  placeholder="Your email adress"
+                  size="lg"
+                />
+                <FormLabel ms="4px" fontSize="sm" fontWeight="normal">
+                  Password
+                </FormLabel>
+                <Input
+                  borderRadius="15px"
+                  mb="36px"
+                  fontSize="sm"
+                  type="password"
+                  placeholder="Your password"
+                  size="lg"
+                />
+
+                <Button
+                  fontSize="10px"
+                  type="submit"
+                  bg="teal.300"
+                  w="100%"
+                  h="45"
+                  mb="20px"
+                  color="white"
+                  mt="20px"
+                  _hover={{
+                    bg: "teal.200",
+                  }}
+                  _active={{
+                    bg: "teal.400",
+                  }}>
+
+                  SIGN IN
+                </Button>
+              </FormControl>
+             
             </Box>
-            
           </Flex>
         </Flex>
-        
       </Flex>
     </Flex>
   );
