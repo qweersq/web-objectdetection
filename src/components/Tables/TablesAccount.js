9b064a1de531d23ac7deaa48d16af5394b7d763d
Andreean26 <adam.andreean@binus.ac.id>
2023-05-07 00:41:57 +0700

    account add create editing edit popup

--- src/components/Tables/TablesAccount.js
+++ src/components/Tables/TablesAccount.js
@@ -19,13 +19,14 @@ import {
     FormErrorMessage,
     FormHelperText,
     Input,
+    Switch,
 } from "@chakra-ui/react";
 import React from "react";
 import { useDisclosure } from "@chakra-ui/react"
 
 function TableAccount(props) {
     const { name, email, role, status } = props;
-    const inputStyle = {border: "1px solid #000",borderRadius: "10px",height: "45px",width: "870px",margin: "15px",};
+    const inputStyle = {border: "1px solid #000",borderRadius: "10px"};
     const textColor = useColorModeValue("gray.700", "white");
     const bgStatus = useColorModeValue("gray.400", "#1a202c");
     const colorStatus = useColorModeValue("white", "gray.400");
@@ -86,27 +87,43 @@ function TableAccount(props) {
                 </Td>
             </Tr>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent w="1000px" h="482px">
-                    <ModalHeader color="green.400">Edit Account</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        
-                        <FormControl id="email">
-                        <FormLabel>Name</FormLabel>
-                        <Input type="text" style={inputStyle} />
-                        
-                        </FormControl>
-                        
-                    </ModalBody>
-                    <ModalFooter justifyContent="center">
-                        <Button bg="green.400" borderRadius="lg" width="870px" onClick={onClose}>
-                            Close
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+            <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                        <ModalOverlay />
+                        <ModalContent maxW="56rem" minW="24.375rem" maxh="482px">
+                            <ModalHeader color="green.400">Edit {name}</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                <Flex justifyContent="space-between">
+                                    <Flex flexDirection="column" width="100%">
+                                        <FormControl>
+                                            <FormLabel htmlFor="name">Name</FormLabel>
+                                            <Input style={inputStyle} id="name" placeholder="Enter name" />
+                                            <FormLabel htmlFor="name" top="150px">Role</FormLabel>
+                                            <Input style={inputStyle} id="role" placeholder="Enter Role" />
+                                            <FormLabel htmlFor="name">Email</FormLabel>
+                                            <Input style={inputStyle} id="email" placeholder="Enter Email" />
+                                            <FormLabel htmlFor="name">Password</FormLabel>
+                                            <Input style={inputStyle} id="password" placeholder="Enter name" />
+                                            <FormLabel htmlFor="name">BINUS</FormLabel>
+                                            <Input style={inputStyle} id="name" placeholder="Enter name" />
+
+                                            <Flex flexDirection="row" alignItems="flex-end">
+                                                <Switch colorScheme="green" size="lg" mt={4} />
+                                                <Text fontSize="md" color={textColor} fontWeight="Bold" pl="12px">
+                                                    Status
+                                                </Text>
+                                            </Flex>
+                                            <Button bg="green.400" color="white" borderRadius="lg" w="847px" mt={4}>
+                                                Edit
+                                            </Button>
+                                        </FormControl>
+                                    </Flex>
+                                </Flex>
+                            </ModalBody>
+                            <ModalFooter justifyContent="center">
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
         </>
     );
 }
--- src/views/Dashboard/Billing/components/Account.js
+++ src/views/Dashboard/Billing/components/Account.js
@@ -7,6 +7,20 @@ import {
   Thead,
   Tr,
   useColorModeValue,
+  useDisclosure,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Switch,
 } from "@chakra-ui/react";
 // Custom components
 import Card from "components/Card/Card.js";
@@ -18,12 +32,54 @@ import React from "react";
 
 const Account = ({ title, captions, data }) => {
   const textColor = useColorModeValue("gray.700", "white");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const inputStyle = {border: "1px solid #000",borderRadius: "10px"};
   return (
     <Card overflowX={{ sm: "scroll", xl: "hidden" }}>
-      <CardHeader p='6px 0px 22px 0px'>
+      <CardHeader p='6px 0px 22px 0px' justifyContent="space-between">
         <Text fontSize='xl' color={textColor} fontWeight='bold'>
           {title}
         </Text>
+        <Button w="65px" h="30px" onClick={onOpen} bg="green.400" color="white" borderRadius="lg" _hover={{ bg: "#00d179" }}>
+                    Create
+        </Button>
+        <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                        <ModalOverlay />
+                        <ModalContent maxW="56rem" minW="24.375rem" maxh="482px">
+                            <ModalHeader color="green.400">Create New Account</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                <Flex justifyContent="space-between">
+                                    <Flex flexDirection="column" width="100%">
+                                        <FormControl>
+                                            <FormLabel htmlFor="name">Name</FormLabel>
+                                            <Input style={inputStyle} id="name" placeholder="Enter name" />
+                                            <FormLabel htmlFor="name">Role</FormLabel>
+                                            <Input style={inputStyle} id="role" placeholder="Enter Role" />
+                                            <FormLabel htmlFor="name">Email</FormLabel>
+                                            <Input style={inputStyle} id="email" placeholder="Enter Email" />
+                                            <FormLabel htmlFor="name">Password</FormLabel>
+                                            <Input style={inputStyle} id="password" placeholder="Enter name" />
+                                            <FormLabel htmlFor="name">Binus</FormLabel>
+                                            <Input style={inputStyle} id="name" placeholder="Enter name" />
+
+                                            <Flex flexDirection="row" alignItems="flex-end">
+                                                <Switch colorScheme="green" size="lg" mt={4} />
+                                                <Text fontSize="md" color={textColor} fontWeight="Bold" pl="12px">
+                                                    Status
+                                                </Text>
+                                            </Flex>
+                                            <Button bg="green.400" color="white" borderRadius="lg" w="847px" mt={4}>
+                                                Create
+                                            </Button>
+                                        </FormControl>
+                                    </Flex>
+                                </Flex>
+                            </ModalBody>
+                            <ModalFooter justifyContent="center">
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
       </CardHeader>
       <CardBody>
         <Table variant='simple' color={textColor}>
