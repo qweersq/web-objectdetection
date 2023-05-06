35995afe4c471cbd0301576da6e0cf8400b37010
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-05-06 21:00:04 +0700

    Pop up Branch

--- src/components/Sidebar/SidebarLocation.js
+++ src/components/Sidebar/SidebarLocation.js
@@ -1,22 +1,30 @@
 import {
     Button,
     Flex,
-    Icon,
-    Spacer,
     Text,
     useColorModeValue,
-    Link
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Grid,
+    Box
+
 } from "@chakra-ui/react";
-import { whiten } from "@chakra-ui/theme-tools";
 import Card from "components/Card/Card";
-import CardBody from "components/Card/CardBody";
 import CardHeader from "components/Card/CardHeader";
 import React from "react";
+import { useDisclosure } from "@chakra-ui/react";
+
+
 
 export function SidebarLocation() {
 
-    const iconTeal = useColorModeValue("teal.300", "teal.300");
     const textColor = useColorModeValue("gray.700", "white");
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <Card p="1rem" my={{ sm: "24px", xl: "0px" }} boxShadow="md">
@@ -31,9 +39,82 @@ export function SidebarLocation() {
                 </Flex>
             </CardHeader>
             <Flex justify="space-between" align="" flexDirection="column" minHeight="30px" w="100%">
-                <Button bg="#00A861" _hover={{ bg: "#00d179" }} size="xs" color={"white"}>
-                    Button
-                </Button>
+                {/* <Button bg="#00A861" _hover={{ bg: "#00d179" }} size="xs" color={"white"}> */}
+                <Button bg="#00A861" _hover={{ bg: "#00d179" }} size="xs" color={"white"} onClick={onOpen}>Open Modal</Button>
+                <Modal onClose={onClose} isOpen={isOpen} isCentered>
+                    <ModalOverlay />
+                    <ModalContent maxW="56rem" w="90%">
+                        <ModalHeader> Change Location <Button bg="#00A861" _hover={{ bg: "#00d179" }} size="sm" color={"white"} ml="20px">Create</Button></ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+                                <Card p="1rem" my={{ sm: "24px", xl: "0px" }} boxShadow="md">
+                                    <CardHeader p="12px 5px" mb="12px">
+                                        <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                            <Text fontSize="md" color={textColor} fontWeight="Bold" justifyContent={"center"}>
+                                                Binus @Malang
+                                            </Text>
+                                        </Flex>
+                                    </CardHeader>
+                                    <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                        <Button size="sm" w="100%">Change Location</Button>
+                                    </Flex>
+                                </Card>
+                                <Card p="1rem" my={{ sm: "24px", xl: "0px" }} boxShadow="md">
+                                    <CardHeader p="12px 5px" mb="12px">
+                                        <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                            <Text fontSize="md" color={textColor} fontWeight="Bold" justifyContent={"center"}>
+                                                Binus @Kemanggisan
+                                            </Text>
+                                        </Flex>
+                                    </CardHeader>
+                                    <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                        <Button size="sm" w="100%" bg="#00A861" _hover={{ bg: "#00d179" }} color={"white"}>Change Location</Button>
+                                    </Flex>
+                                </Card>
+                                <Card p="1rem" my={{ sm: "24px", xl: "0px" }} boxShadow="md">
+                                    <CardHeader p="12px 5px" mb="12px">
+                                        <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                            <Text fontSize="md" color={textColor} fontWeight="Bold" justifyContent={"center"}>
+                                                Binus @Alam Sutera
+                                            </Text>
+                                        </Flex>
+                                    </CardHeader>
+                                    <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                        <Button size="sm" w="100%" bg="#00A861" _hover={{ bg: "#00d179" }} color={"white"}>Change Location</Button>
+                                    </Flex>
+                                </Card>
+                                <Card p="1rem" my={{ sm: "24px", xl: "0px" }} boxShadow="md">
+                                    <CardHeader p="12px 5px" mb="12px">
+                                        <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                            <Text fontSize="md" color={textColor} fontWeight="Bold" justifyContent={"center"}>
+                                                Binus @Bandung
+                                            </Text>
+                                        </Flex>
+                                    </CardHeader>
+                                    <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                        <Button size="sm" w="100%" bg="#00A861" _hover={{ bg: "#00d179" }} color={"white"}>Change Location</Button>
+                                    </Flex>
+                                </Card>
+                                <Card p="1rem" my={{ sm: "24px", xl: "0px" }} boxShadow="md">
+                                    <CardHeader p="12px 5px" mb="12px">
+                                        <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                            <Text fontSize="md" color={textColor} fontWeight="Bold" justifyContent={"center"}>
+                                                Binus @Semarang
+                                            </Text>
+                                        </Flex>
+                                    </CardHeader>
+                                    <Flex justify="space-between" align="center" flexDirection="column" minHeight="30px" w="100%">
+                                        <Button size="sm" w="100%" bg="#00A861" _hover={{ bg: "#00d179" }} color={"white"}>Change Location</Button>
+                                    </Flex>
+                                </Card>
+                            </Grid>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button onClick={onClose}>Close</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
             </Flex>
         </Card>
     );
