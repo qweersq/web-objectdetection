e6cdc157b4f99f0745b8039629483601327478ba
Andreean26 <adam.andreean@binus.ac.id>
2023-05-18 18:27:41 +0700

    the new history

--- src/views/Dashboard/Tables/components/History.js
+++ src/views/Dashboard/Tables/components/History.js
@@ -1,62 +1,75 @@
-// Chakra imports
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import {
-    Table,
-    Tbody,
-    Text,
-    Th,
-    Thead,
-    Tr,
-    useColorModeValue,
-  } from "@chakra-ui/react";
-  // Custom components
-  import Card from "components/Card/Card.js";
-  import CardBody from "components/Card/CardBody.js";
-  import CardHeader from "components/Card/CardHeader.js";
-  import TablesHistory from "components/Tables/TablesHistory";
-  import TablesTableRow from "components/Tables/TablesTableRow";
-  import React from "react";
-  
-  const History = ({ title, captions, data }) => {
-    const textColor = useColorModeValue("gray.700", "white");
-    return (
-      <Card overflowX={{ sm: "scroll", xl: "hidden" }}>
-        <CardHeader p='6px 0px 22px 0px'>
-          <Text fontSize='xl' color={textColor} fontWeight='bold'>
-            {title}
-          </Text>
-        </CardHeader>
-        <CardBody>
-          <Table variant='simple' color={textColor}>
-            <Thead> 
-              <Tr my='.8rem' pl='0px' color='gray.400'>
-                {captions.map((caption, idx) => {
-                  return (
-                    <Th color='gray.400' key={idx} ps={idx === 0 ? "0px" : null}>
-                      {caption}
-                    </Th>
-                  );
-                })}
-              </Tr>
-            </Thead>
-            <Tbody>
-              {data.map((row) => {
+  Table,
+  Tbody,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+import TablesHistory from "components/Tables/TablesHistory";
+import TablesTableRow from "components/Tables/TablesTableRow";
+
+const History = ({ title, captions }) => {
+  const textColor = useColorModeValue("gray.700", "white");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/history");
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Card overflowX={{ sm: "scroll", xl: "hidden" }}>
+      <CardHeader p="6px 0px 22px 0px">
+        <Text fontSize="xl" color={textColor} fontWeight="bold">
+          {title}
+        </Text>
+      </CardHeader>
+      <CardBody>
+        <Table variant="simple" color={textColor}>
+          <Thead>
+            <Tr my=".8rem" pl="0px" color="gray.400">
+              {captions.map((caption, idx) => {
                 return (
-                  <TablesHistory
-                    sensor={row.sensor}
-                    security={row.security}
-                    jenis={row.jenis}
-                    status={row.status}
-                    jam={row.jam}
-                    tanggal={row.tanggal}
-                  />
+                  <Th color="gray.400" key={idx} ps={idx === 0 ? "0px" : null}>
+                    {caption}
+                  </Th>
                 );
               })}
-            </Tbody>
-          </Table>
-        </CardBody>
-      </Card>
-    );
-  };
-  
-  export default History;
-  
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data.map((row) => {
+              return (
+                <TablesHistory
+                  sensor={row.sensor}
+                  security={row.security}
+                  jenis={row.jenis}
+                  status={row.status}
+                  jam={row.jam}
+                  tanggal={row.tanggal}
+                />
+              );
+            })}
+          </Tbody>
+        </Table>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default History;
